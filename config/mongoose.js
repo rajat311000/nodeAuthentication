3aea68bf9a4aa3739f662aa475b55466f1d4b4f0
rajat311000 <130726733+rajat311000@users.noreply.github.com>
2023-07-11 22:26:15 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 
-mongoose.connect(`mongodb://127.0.0.1:27017/${process.env.DB}`);
+mongoose.connect(process.env.DB_URL);
 
 const db = mongoose.connection;
 
